1248ae29802b06eb7996f1100101494971f84f1c
akash-bt21cse024 <146684897+akash-bt21cse024@users.noreply.github.com>
2024-06-02 23:13:39 +0530

    empty wishlist css

--- src/pages/Card/index.js
+++ src/pages/Card/index.js
@@ -9,12 +9,22 @@ export const Cardpage = () => {
   return (
     <div>
       <Header></Header> <main className="mt-20">
-      <div className="flex flex-wrap gap-3 ml-56">
+      <div className="flex flex-wrap gap-3 ml-12">
   {card?.length > 0 ? (
     card.map((product) => <Card key={product._id} product={product} />)
-  ) : (<div>
-    <h1>There is no item in wishlist</h1>
-    <button onClick={()=>navigate("/")} className="border drop-shadow bg-cyan-200 mt-6"> click add to wishlist</button>
+  ) : (
+    <div className="flex items-center justify-center w-full">
+      <div className="flex  items-center justify-center border bg-cyan-50 drop-shadow-2xl w-1/2 h-[32rem]">
+        <div className="flex flex-col h-[16rem] w-1/2 border drop-shadow-2xl bg-cyan-100 items-center ">
+          <h1 className="my-8">---YOUR CART IS EMPTY---</h1>
+          <button
+            className="my-2 h-12 w-[12rem] rounded-lg border text-center drop-shadow-2xl bg-cyan-300 hover:opacity-50"
+            onClick={() => navigate("/")}
+          >
+            Click to add in cart
+          </button>
+        </div>
+      </div>
     </div>
   )}
 </div>
--- src/pages/Wishlist/index.js
+++ src/pages/Wishlist/index.js
@@ -1,27 +1,38 @@
 import { Header } from "../../component/Header";
-import {Wishlist} from "../../component/Wishlist";
+import { Wishlist } from "../../component/Wishlist";
 
 import { useWishlist } from "../../context/wishlist.context";
 import { useNavigate } from "react-router-dom";
-export const Wishlistpage = () => {
 
- const navigate=useNavigate()
-  const{wishlist}=useWishlist();
+export const Wishlistpage = () => {
+  const navigate = useNavigate();
+  const { wishlist } = useWishlist();
 
   return (
     <div>
-      <Header></Header><main className="mt-16">
-      <div className="flex flex-wrap gap-3 ml-56 ">
-        {wishlist?.length > 0 ? (
-          wishlist.map((product) => (
-            <Wishlist key={product._id} product={product} />
-          ))
-        ) : 
-        <div className="flex flex-col items-center "><div><p className="">wishlist is empty</p></div>
-        <button className="my-6 border drop-shadow-xl w-32 bg-cyan-200" onClick={()=>navigate("/")}>click add to wishlist</button></div>
-        }
-      </div>
-      
+      <Header />
+      <main className="mt-16">
+        <div className="flex flex-wrap gap-3 ml-12">
+          {wishlist?.length > 0 ? (
+            wishlist.map((product) => (
+              <Wishlist key={product._id} product={product} />
+            ))
+          ) : (
+            <div className="flex items-center justify-center w-full">
+              <div className="flex  items-center justify-center border bg-cyan-50 drop-shadow-2xl w-1/2 h-[32rem]">
+                <div className="flex flex-col h-[16rem] w-1/2 border drop-shadow-2xl bg-cyan-100 items-center ">
+                  <h1 className="my-8">--- YOUR WISHLIST IS EMPTY---</h1>
+                  <button
+                    className="my-2 h-12 w-[12rem] rounded-lg border text-center drop-shadow-2xl bg-cyan-300 hover:opacity-50"
+                    onClick={() => navigate("/")}
+                  >
+                    Click to add in wishlist
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
       </main>
     </div>
   );
