7a4e2810565f779f0258533a1df713b2568274de
akash-bt21cse024 <146684897+akash-bt21cse024@users.noreply.github.com>
2024-06-06 21:49:06 +0530

    improve authentication and toast

--- src/component/Login/index.js
+++ src/component/Login/index.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 import { useCard } from "../../context/card.context";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+
+
 export  const Login= ()=>{
  const [number,setnumber]=useState("")
  const [password,setpassword]=useState("")
 const {setsignup,setuserid}=useCard();
- 
+const[eye,seteye]=useState(false) ;
 const navigate=useNavigate();
     const sign =async ()=>{
         
@@ -39,11 +41,16 @@ const navigate=useNavigate();
     return( <div className=" flex  justify-center items-center ">
         <div className="flex  items-center flex-col   drop-shadow-2xl w-1/2 h-[32rem] border  bg-cyan-50 ">
           <h1 className="my-12 ">-------LOGIN PAGE------</h1>
-          <div className=" w-1/2 items-center h-[16rem]  drop-shadow-2xl border bg-cyan-100 ">
+          <div className=" w-[25rem] items-center h-[16rem]  drop-shadow-2xl border bg-cyan-100 ">
         <nav className="flex flex-col">
         
-        <div className=" my-2  border  drop-shadow-2xl bg-cyan-100"><label className="mx-4">password--</label><input onChange={(e)=>{setpassword(e.target.value)}} required type="password" placeholder="password"></input> </div>
-        <div className=" my-2  border  drop-shadow-2xl bg-cyan-100"><label className="mx-4">number--</label> <input onChange={(e)=>{setnumber(e.target.value)}} required placeholder="number"></input></div>
+        <div className=" my-2 flex   border  drop-shadow-2xl bg-cyan-100"><label className="mx-4 w-[10rem]">password--</label> <div className="mx-4 w-full "> <input className="w-full relative" onChange={(e)=>{setpassword(e.target.value)}} required type={eye?"text":"password"} placeholder="password"></input> <button onClick={()=>seteye((flag)=>!flag)} className="flex absolute right-4 top-0">
+         { eye?<span class="material-symbols-outlined">
+visibility
+</span>:<span class="material-symbols-outlined">
+visibility_off
+</span>} </button> </div></div>
+        <div className=" my-2  border flex drop-shadow-2xl bg-cyan-100"><label className="mx-4 w-[10rem]">number--</label> <input className="w-full mx-4" onChange={(e)=>{setnumber(e.target.value)}} required placeholder="number"></input></div>
         <div className="  "> <button className="my-2 h-8 w-56 ml-16 rounded-lg border text-center drop-shadow-2xl bg-cyan-300 hover:opacity-50" onClick={()=>sign()  } > Login </button></div></nav>
      <div className=" "><button className="my-2 ml-16 h-8 w-56 rounded-lg border text-center drop-shadow-2xl bg-cyan-300 hover:opacity-50"  onClick={testcredencial}>Login for credencial test </button></div>
        <div className="  "> <button className=" my-2 ml-16 h-8 w-56 rounded-lg border text-center drop-shadow-2xl bg-cyan-300 hover:opacity-50" onClick={()=>setsignup(true)}  > Create new account </button></div>
--- src/component/SignUp/index.js
+++ src/component/SignUp/index.js
@@ -9,12 +9,7 @@ import { toast } from "react-toastify"
   const [password,setpassword]=useState("")
   const [number,setnumber]=useState("")
   const {setsignup}=useCard();
-  // const sign =async ()=>{
-    
-  //   const data=await signupfxn({name,email,password,number});
-  //   console.log("----------------------------------------------",data,typeof(data))
-  // return data;
-  // }
+  const[eye,seteye]=useState(false)
   const nameRegx=new RegExp(/[A-Za-z]{3,}/);
 const passwordRegx= new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/)
 const numberRegx=new RegExp(/^[\d]{10}$/);
@@ -27,7 +22,7 @@ const submit = async()=>{
                 }else if(!numberRegx.test(number)){ toast.warning("number is not valid");}
                   else{
                     const test=await signupfxn({name,email,password,number});
-                    console.log("________________test___________",test,typeof(test))
+                    
                     if(test)
                     {toast.success("Account is created ")
                       setsignup(false)
@@ -45,7 +40,12 @@ const submit = async()=>{
             <div className="flex flex-col h-[16rem] w-1/2 border drop-shadow-2xl bg-cyan-100  justify-center  ">
               <div className=" my-2 border  drop-shadow-2xl bg-cyan-100 "><label className="mx-4">name--</label> <input onChange={(e)=>{setname(e.target.value)}} required placeholder="Name"></input> </div>
               <div className=" my-2  border drop-shadow-2xl bg-cyan-100" ><label className="mx-4">email--</label> <input onChange={(e)=>{setemail(e.target.value)}} required placeholder="email"></input></div>
-              <div className=" my-2  border  drop-shadow-2xl bg-cyan-100"><label className="mx-4">password--</label><input onChange={(e)=>{setpassword(e.target.value)}} required type="password" placeholder="password"></input> </div>
+              <div className=" my-2 flex   border  drop-shadow-2xl bg-cyan-100"><label className="mx-4 w-[10rem]">password--</label> <div className="mx-4 w-full "> <input className="w-full relative" onChange={(e)=>{setpassword(e.target.value)}} required type={eye?"text":"password"} placeholder="password"></input> <button onClick={()=>seteye((flag)=>!flag)} className="flex absolute right-4 top-0">
+         { eye?<span class="material-symbols-outlined">
+visibility
+</span>:<span class="material-symbols-outlined">
+visibility_off
+</span>} </button> </div></div>
               <div className=" my-2  border  drop-shadow-2xl bg-cyan-100"><label className="mx-4">number--</label> <input onChange={(e)=>{setnumber(e.target.value)}} required placeholder="number"></input></div>
               <div className=" flex justify-between "> <button className="my-2 h-8 w-20 ml-4 rounded-lg border  text-center drop-shadow-2xl bg-cyan-300 hover:opacity-50" onClick={()=>submit()} type="submit"> submit </button>
              <button className="my-2 h-8 w-36 rounded-lg border mr-4 text-center drop-shadow-2xl bg-cyan-300 hover:opacity-50" onClick={()=>{ 
